84434d9c32cf0fa158440bf3432bde6bcb29b421
MartinDeep13 <martin0013esp@hotmail.com>
2019-02-15 14:21:25 -0600

    registro y verificacion correo

--- app/src/main/java/com/example/aplicaciones3/limpirepartidor/registro.java
+++ app/src/main/java/com/example/aplicaciones3/limpirepartidor/registro.java
@@ -1,18 +1,40 @@
 package com.example.aplicaciones3.limpirepartidor;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class registro extends AppCompatActivity implements View.OnClickListener {
-
+    EditText correo, contrasena;
+    Button registro;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener stateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        FirebaseAuth.getInstance().signOut();
+        mAuth = FirebaseAuth.getInstance();
+
         findViewById(R.id.cerrar).setOnClickListener(this);
         findViewById(R.id.registro).setOnClickListener(this);
+
+
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contraseña);
+
     }
 
     @Override
@@ -24,9 +46,34 @@ public class registro extends AppCompatActivity implements View.OnClickListener 
                 startActivity(intent);
                 break;
             case R.id.registro:
-                Intent regi = new Intent(registro.this,Recovery.class);
-                startActivity(regi);
+                registro();
                 break;
         }
     }
+
+    public void registro()
+    {
+        final String
+                Correo = String.valueOf(correo.getText()),
+                Contra = String.valueOf(contrasena.getText());
+        if (!Correo.isEmpty() && !Contra.isEmpty()) {
+            mAuth.createUserWithEmailAndPassword(Correo,Contra).addOnCompleteListener(registro.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        user.sendEmailVerification();
+                        Intent regi = new Intent(registro.this,Recovery.class);
+                        startActivity(regi);
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(registro.this, "" + e, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
 }
