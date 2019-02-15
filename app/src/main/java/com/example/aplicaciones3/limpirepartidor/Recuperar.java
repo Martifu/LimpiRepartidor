476a8a7b7c9d5c66166e2bba0054f0222f9627d8
keggo97 <eherrera0597@hotmail.com>
2019-02-15 14:46:47 -0600

    recovery

--- app/src/main/java/com/example/aplicaciones3/limpirepartidor/Recuperar.java
+++ app/src/main/java/com/example/aplicaciones3/limpirepartidor/Recuperar.java
@@ -1,21 +1,35 @@
 package com.example.aplicaciones3.limpirepartidor;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Recuperar extends AppCompatActivity implements View.OnClickListener {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class Recuperar extends AppCompatActivity  {
+    private EditText correo;
+    private Button bt1, close;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar);
-        findViewById(R.id.cerrar).setOnClickListener(this);
-        findViewById(R.id.enviar).setOnClickListener(this);
+        correo=(EditText) findViewById(R.id.recovery_correo);
+        bt1=(Button) findViewById(R.id.recovery_enviar);
+        close=(Button) findViewById(R.id.recovery_cerrar);
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
 
-    @Override
+   /* @Override
     public void onClick(View v) {
         switch (v.getId())
         {
@@ -28,5 +42,39 @@ public class Recuperar extends AppCompatActivity implements View.OnClickListener
                 startActivity(enviar);
                 break;
         }
+    }*/
+
+    public void send(View view) {
+        String userEmail = correo.getText().toString();
+        if(TextUtils.isEmpty(userEmail))
+        {
+            Toast.makeText(Recuperar.this,"Ingrese el correo",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+
+            firebaseAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(Recuperar.this,"el correo fue enviado exitosamente", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(Recuperar.this,Login.class));
+                    }
+                    else
+                    {
+                        String mensaje= task.getException().getMessage();
+                        Toast.makeText(Recuperar.this,"Error: "+mensaje,Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    public void cerrar(View view) {
+        Intent cerraer = new Intent(Recuperar.this,Login.class);
+        startActivity(cerraer);
+
     }
 }
